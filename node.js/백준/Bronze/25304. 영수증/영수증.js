b53c668051d2063dba89e31b39146b88b599456c
Ding <dnwnd501@naver.com>
2026-03-04 00:29:17 +0900

    [Bronze IV] Title: 영수증, Time: 88 ms, Memory: 9352 KB -BaekjoonHub

--- node.js/백준/Bronze/25304. 영수증/영수증.js
+++ node.js/백준/Bronze/25304. 영수증/영수증.js
@@ -3,13 +3,12 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
 const totalPrice = parseInt(input[0]);
-const totalGoods = parseInt(input[1]);
+const goodsList = input.slice(2);
 
-const calculatePrice = Array.from({ length: totalGoods }, (_v, i) => i + 2)
-  .map((i) => {
-    const [price, count] = input[i].split(" ");
-    return parseInt(price) * parseInt(count);
-  })
-  .reduce((acc, cur) => acc + cur, 0);
+const calculatePrice = goodsList.reduce((acc, line)=>{
+    if(!line) return acc;
+    const [price, qty] = line.split(" ").map(Number);
+    return acc + (price * qty);
+},0);
 
-console.log(totalPrice === calculatePrice ? "Yes" : "No")
+console.log(totalPrice === calculatePrice ? "Yes" : "No");
