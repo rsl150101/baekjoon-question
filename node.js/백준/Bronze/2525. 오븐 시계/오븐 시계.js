00aab1b0c666cfa3dbe3cd107bb22ee1681e7fd8
Ding <dnwnd501@naver.com>
2026-03-03 18:19:25 +0900

    [Bronze III] Title: 오븐 시계, Time: 88 ms, Memory: 9332 KB -BaekjoonHub

--- node.js/백준/Bronze/2525. 오븐 시계/오븐 시계.js
+++ node.js/백준/Bronze/2525. 오븐 시계/오븐 시계.js
@@ -7,10 +7,8 @@ const A = parseInt(timeArr[0]);
 const B = parseInt(timeArr[1]);
 const C = parseInt(input[1]);
 
-if(B+C >=60){
-    const overMin = Math.trunc((B+C)/60)
-    const hour = (A + overMin) >= 24 ? A + overMin - 24 : A + overMin 
-    console.log(`${hour} ${B+C-(overMin * 60)}`)
-}else{
-    console.log(`${A} ${B+C}`)
-}
+const totalMin = A *60 + B + C;
+const hour = Math.trunc(totalMin/60)%24
+const min = totalMin%60
+
+console.log(`${hour} ${min}`)
