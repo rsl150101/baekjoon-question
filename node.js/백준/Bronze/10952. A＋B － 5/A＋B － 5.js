5896d655ef5bcfd20d74841c4a9ce710db93d581
Ding <dnwnd501@naver.com>
2026-03-10 16:06:11 +0900

    [Bronze V] Title: A+B - 5, Time: 88 ms, Memory: 9360 KB -BaekjoonHub

--- node.js/백준/Bronze/10952. A＋B － 5/A＋B － 5.js
+++ node.js/백준/Bronze/10952. A＋B － 5/A＋B － 5.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
 
-const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const testArr = input.slice(0,-1)
 let result = ''
 
-testArr.forEach((v) => {
+input.forEach((v) => {
   const [A, B] = v.split(" ").map(Number);
-  if(A+B===0) return;
+  if(A + B === 0) return
   result += (A + B) + '\n';
 });
+    
+
 
 console.log(result)
