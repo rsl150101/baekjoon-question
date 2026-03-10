cea1ad1e107f065f76f5b45620efdc133b4eb5fc
Ding <dnwnd501@naver.com>
2026-03-10 15:38:01 +0900

    [Bronze V] Title: A+B - 7, Time: 92 ms, Memory: 9380 KB -BaekjoonHub

---
+++ node.js/백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const testArr = input.slice(1)
+let result = ''
+
+testArr.forEach((v, i) => {
+  if(!v) return;
+  const [A, B] = v.split(" ").map(Number);
+  result += `Case #${i+1}: ` + (A + B) + '\n';
+});
+
+console.log(result)
