7db0ab83860f8965dba947a8de3d0286815b67c0
Ding <dnwnd501@naver.com>
2026-04-05 20:13:24 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 92 ms, Memory: 9352 KB -BaekjoonHub

--- node.js/백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
+++ node.js/백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
@@ -4,17 +4,8 @@ let input = fs.readFileSync(0).toString().trim();
 
 const croatiaAlphabet = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="];
 
-let count = 0;
+for (let alphabet of croatiaAlphabet) {
+  input = input.replaceAll(alphabet, "*");
+}
 
-croatiaAlphabet.forEach((v) => {
-  while (input.includes(v)) {
-    input = input.replace(v, "+");
-    count += 1;
-  }
-});
-
-input = input.replaceAll("+", "");
-
-count += input.split("").length;
-
-console.log(count);
+console.log(input.length);
