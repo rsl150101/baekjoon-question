1e54d630e999f7bed81c3df6684942ccf8333029
Ding <dnwnd501@naver.com>
2026-04-11 21:50:02 +0900

    [Bronze II] Title: 진법 변환, Time: 148 ms, Memory: 9644 KB -BaekjoonHub

--- node.js/백준/Bronze/2745. 진법 변환/진법 변환.js
+++ node.js/백준/Bronze/2745. 진법 변환/진법 변환.js
@@ -1,21 +1,4 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split(' ');
 
-const num = input[0].split("").reverse();
-const base = Number(input[1]);
-
-let result = 0;
-
-for (let i = 0; i < num.length; i++) {
-  let char = num[i];
-  let value;
-    
-  if(char >= 'A' && char <= 'Z'){
-    value = char.charCodeAt(0) - 55
-  }else{
-      value = Number(char)
-  }
-  result += value * Math.pow(base, i);
-}
-
-console.log(result);
+console.log(parseInt(input[0], Number(input[1])));
